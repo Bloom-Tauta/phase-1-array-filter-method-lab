a4d1710a693bedc92b85643e40f42177d9803efd
Bloom <bloomtauta@gmail.com>
2022-12-15 14:18:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ["John", "Tim", "Lucy"];
+
+function findMatching(drivers, John){
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === John.toLowerCase()
+    });
+};
+
+function fuzzyMatch(drivers, John){
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, John.length) === John.toLowerCase()
+
+    });
+};
+function matchName(drivers, Lucy) {
+    return drivers.filter(function (driver) {
+        return driver.name === Lucy
+    });
+};
